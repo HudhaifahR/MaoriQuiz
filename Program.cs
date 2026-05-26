1e6151d7537713b554e93cfc8ed2644b2809fcc1
HudhaifahR <ac166694@avcol.school.nz>
2026-05-26 13:52:02 +1200

    I made my welcome message that asks for their first name and last name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string Firstname, Lastname;
+
+            Console.WriteLine("Hello please enter your first name");
+            Firstname = Console.ReadLine();
+            Console.WriteLine("please enter your last name");
+            Lastname = Console.ReadLine();  
+            Console.WriteLine("Hello " + Firstname + " " + Lastname + "!");
+
         }
     }
 }
